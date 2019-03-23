de43373754d30d1e2ee85c58533bd4c2c411dd7e
ChandanNaik999 <chandannaik999@gmail.com>
2019-03-23 14:05:07 +0530

    first addition

---
+++ src/launcher/Main.java
@@ -0,0 +1,27 @@
+package launcher;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int op;
+		Scanner in = new Scanner(System.in);
+		
+		
+		do {
+			op = in.nextInt();
+			switch(op) {
+				case 1: //function calls
+					break;
+				case 2: //...
+					break;
+				//...
+			}
+		}while(op != 11);
+
+		in.close();
+	}
+
+}
